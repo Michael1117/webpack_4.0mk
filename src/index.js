28897a0408d49038e9a4ab5798446fc629a16021
Michael1117 <1322720377@qq.com>
2019-03-25 16:50:38 +0800

    add createAvatar.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import avatar from './avatar.jpg'
 //var avatar = require('./avatar.jpg')
-import './index.scss'
+import style from './index.scss'
+//import './index.scss'
+import createAvatar from './createAvatar'
+
+
+createAvatar()
 
 var img = new Image();
 img.src = avatar
-img.classList.add('avatar')
+img.classList.add(style.avatar)
 
 var root = document.getElementById('root');
 root.append(img)
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,13 @@ module.exports = {
             test: /\.scss$/,
             use: [
                 'style-loader',
-                'css-loader', 
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 2,
+                        modules: true
+                    }
+                },
                 'sass-loader',
                 'postcss-loader'
             ]
