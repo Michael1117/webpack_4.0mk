c68241f5d0d4af87d2e32be9d4343ff9c15e2328
Michael1117 <1322720377@qq.com>
2019-03-26 19:36:58 +0800

    代码拆分之后

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@
     当业务逻辑发生变化时 又要加载2mb的内容
 
 */
-import _ from 'lodash'
-
 // 业务逻辑 1Mb
 console.log(_.join(['a','b','c'], '***'))
 
 // 省略10000行业务逻辑代码
 console.log(_.join(['a','b','c'], '***'))
-//
+// 第二种方式
+
+// main.js被拆分成 lodash.js(1Mb)，main.js(1Mb)
+// 当页面业务逻辑发生变化时，只要加载main.js即可(1Mb)
 
