d4d897bc27a3ce5f35ae7bef248685cdef129886
Michael1117 <1322720377@qq.com>
2019-03-26 17:38:17 +0800

    react

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const webpack = require('webpack');
 
 
-
 module.exports = {
     mode: 'development',
     devtool: 'cheap-module-eval-source-map',
